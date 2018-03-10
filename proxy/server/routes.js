6dc68f181a571136c2529caefeaa67077c7fe56e
jsonbao <jsonbao@gmail.com>
2018-03-09 18:29:55 -0800

    Added console logs to router

--- proxy/server/routes.js
+++ proxy/server/routes.js
@@ -2,22 +2,27 @@ const router = require('express').Router();
 const request = require('request');
 
 router.get('/load', (req, res) => {
+  console.log('load');
   request('http://expanded:3005/load').pipe(res);
 });
 
 router.get('/description', (req, res) => {
+  console.log('description');
   request('http://description:3001/description').pipe(res);
 });
 
 router.get('/feedback', (req, res) => {
+  console.log('feedback');
   request('http://feedback:3002/feedback').pipe(res);
 });
 
 router.get('/courses', (req, res) => {
+  console.log('courses');
   request('http://payment:3003/courses').pipe(res);
 });
 
 router.get('/compare', (req, res) => {
+  console.log('compare');
   request('http://compare:3004/compare', { qs: req.query }).pipe(res);
 });
 module.exports = router;
